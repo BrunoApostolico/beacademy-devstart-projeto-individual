beeaa8e45caafcb45e08dc3c227ce1cdb3607f97
BRUNO APOSTOLICO <bapostolico@gmail.com>
2022-06-30 15:45:38 -0300

    Create Clients Migrate

---
+++ projeto-individual-beneficios/database/migrations/2022_06_30_121644_create_clients_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name',80);
+            $table->date('date_birth');
+            $table->string('cpf',11);
+            $table->string('rg',10);
+            $table->integer('id_address');
+            $table->integer('address_number');
+            $table->string('address_complement',10);
+            $table->string('phone1',12);
+            $table->string('phone2',12);
+            $table->string('email',20);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
