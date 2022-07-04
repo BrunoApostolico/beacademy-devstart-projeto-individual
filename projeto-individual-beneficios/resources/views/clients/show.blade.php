f9b54c739d27900451f46df12c3fd09c05ece12f
Bruno Apostolico <bapostolico@gmail.com>
2022-07-04 18:48:07 -0300

    Blade

--- projeto-individual-beneficios/resources/views/clients/show.blade.php
+++ projeto-individual-beneficios/resources/views/clients/show.blade.php
@@ -1,15 +1,7 @@
-<!doctype html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cliente {{ $client->name }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
+@extends('template.users')
+@section('title',"Cliente {$client->name}")
+@section('body')
+
     <h1>Cliente {{ $client->name }}</h1>
 
     <table class="table">
@@ -41,6 +33,4 @@
 
         </tbody>
     </table>
-</div>
-</body>
-</html>
+@endsection
